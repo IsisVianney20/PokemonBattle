54c7a7c682a1c387f3839014e8ac9ca1629f910b
IsisVianney20 <mviann479@gmail.com>
2025-06-13 17:48:33 -0600

    Scriptable Objects

---
+++ Assets/Scripts/Fighter.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Fighter : MonoBehaviour
+{
+    [SerializeField]
+    private Health _health;
+
+    [SerializeField]
+    private Animator characterAnimator;
+
+    [SerializeField]
+    private Attacks _attacks;
+}
---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField]
+    private Slider _healthSlider;
+
+    [SerializeField]
+    private float _initialHealth = 200f;
+
+    [SerializeField]
+    private UnityEvent<float> _onUpdateHealth;
+
+    [SerializeField]
+    private UnityEvent _onDefeated;
+
+    [SerializeField]
+    private UnityEvent _onTakeDamage;
+
+    private float _currentHeath;
+
+    public void InitialHealth()
+    {
+        _currentHeath = _initialHealth;
+        UpdateHealth();
+    }
+
+    private void UpdateHealth()
+    {
+        _onUpdateHealth?.Invoke(_currentHeath / _initialHealth);
+    }
+
+    public void TakeDamage(float damage)
+    {
+        _currentHeath -= damage;
+        if (_currentHeath < 0)
+        {
+            _onDefeated?.Invoke();
+            _currentHeath = 0;
+        }
+        else
+        {
+            _onTakeDamage?.Invoke();
+        }
+        UpdateHealth();
+    }
+}
